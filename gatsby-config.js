ec6e8cc24ebb78287ab221735f51b1a330547de1
authorcwmoon <williams.e.christian@gmail.com>
2020-09-26 15:39:19 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,6 @@ module.exports = {
     title: `The frequency of tales.`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `cwmoon`,
-    pathPrefix: "",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
