e3c1630a3d6eacd72bc601212f318b8a66f44087
Christian Williams <christian@MacBook-Pro.local>
2020-09-26 15:56:28 -0500

    yes

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: `The frequency of tales.`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `cwmoon`,
+    pathPrefix: '/journal',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
