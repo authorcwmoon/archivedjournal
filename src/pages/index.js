0f9ae7fc80814cafc1a8bb1cd80035142899c5c6
Christian Williams <christian@MacBook-Pro.local>
2020-09-27 20:36:56 -0500

    commit on master

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Helmet } from "react-helmet"
+// import { Helmet } from "react-helmet"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+// import Layout from "../components/layout"
+// import Image from "../components/image"
+// import SEO from "../components/seo"
 
 export default function Index({ data }) {
   const { edges: posts } = data.allMarkdownRemark
