32f9248ea51c8c7e9da64dcc9af0bac1fef98462
Christian Williams <christian@MacBook-Pro.local>
2020-10-03 16:30:24 -0500

    all

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,111 +1,56 @@
 module.exports = {
   siteMetadata: {
-    title: `The frequency of tales.`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `cwmoon`,
-
+    title: "The Journal of C.W. Moon",
+    description: "The frequency of stories",
+    url: "https://cwmoon.com",
+    image:"/office.jpg",
+    twitterUsername: "@cwill44",
+    author: "CWMoons",
   },
   plugins: [
-    'gatsby-plugin-catch-links',
-    `gatsby-plugin-react-helmet`,
-
-    
-    {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [] // just in case those previously mentioned remark plugins sound cool :)
-      },
-    },
-
-
-
-
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `pages`,
         path: `${__dirname}/src/pages`,
-        name: "pages"
       },
     },
-
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `posts`,
         path: `${__dirname}/src/posts`,
-        name: "posts"
       },
     },
-
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `images`,
         path: `${__dirname}/src/images`,
-        name: "images"
       },
     },
-
-
-    `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
-        extensions: [`.mdx`, `.md`],
+        extensions: [`.md`, `.mdx`],
         gatsbyRemarkPlugins: [
           {
-            resolve: 'gatsby-remark-images',
+            resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 1200,
             },
           },
-
-
+        ],
+      },
+    },
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [
-          `Libre Baskerville`,
-          `muli\:300,400,400i,700` // you can also specify font weights and styles
-        ],
-        display: 'swap'
+        fonts: [`roboto mono`, `muli\:400,400i,700,700i`],
+        display: "swap",
       },
     },
-
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `images`,
-    //     path: `${__dirname}/src/images`,
-    //   },
-    // },
-
-    
-
-
-    
-        ],
-      },
-    },
-
-    
-
-    `gatsby-plugin-styled-components`,
-   
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,65 +1,6 @@
-// Post.js
-import React from "react";
-import { Link } from "gatsby";
-import "./post.css";
+import React from "react"
+import { PostWrapper } from "../elements"
 
-const Post = ({ title, author, date, description, path }) => (
-  <div className="post">
-    <h3 className="post-title">{ title }</h3>
-    <p className="post-description">{ description }</p>
-    <p className="post-written-by">
-      Written by { author } on { date }
-    </p>
-    <Link to={ path }>Read more</Link>
-  </div>
-);
-
-/* SectionLayout */
-
-const SectionLayout = ({ children, alignContent,maxWidth }) => {
-  return (
-    <Layout alignContent={alignContent} >
-      <InnerContainer maxWidth={maxWidth}>{children}</InnerContainer>
-    </Layout>
-  )
+export const Post = ({ children }) => {
+  return <PostWrapper>{children}</PostWrapper>
 }
-
-const InnerContainer = styled.div`
-  width: 100%;
-  max-width: ${props => props.maxWidth};
-  margin: 0px auto;
-  position: relative;
-  padding: 32px 0 0 0;
-  @media ${device.laptop} {
-    padding: 32px 0 0 0;
-  }
-  @media ${device.mobileL} {
-    padding: 0;
-  }
-`
-
-const Layout = styled.div`
-  max-width: 1600px;
-  text-align: ${props => props.alignContent};
-  @media ${device.desktop} {
-    max-width: 1300px;
-    margin: 0 auto;
-  }
-  @media ${device.laptopL} {
-    max-width: 1200px;
-    margin: 0 auto;
-    padding: 0 64px;
-  }
-  @media ${device.tablet} {
-    padding: 0;
-  }
-`
-
-export default SectionLayout
-
-
-
-
-
-
-export default Post;
