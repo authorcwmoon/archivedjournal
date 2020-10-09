00bff0e4731b0bbc715ca54c3160880f09a7e3e9
Christian Williams <christian@MacBook-Pro.local>
2020-10-09 17:45:22 -0500

    netform :

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -15,113 +15,19 @@ import { SubscribeWrapper, P, H2 } from "../elements";
 
 
 export const Subscribe = () => {
-  // const [loading, setLoading] = useState(false);
-  // const inputEl = useRef(null);
-  // const toast = useToast();
-  // const { colorMode } = useColorMode();
-  // const bgColor = {
-  //   light: 'blue.50',
-  //   dark: 'awblue.900'
-  // };
-  // const borderColor = {
-  //   light: 'blue.200',
-  //   dark: 'blue.900'
-  // };
-
-  // const subscribe = async (e) => {
-  //   e.preventDefault();
-  //   setLoading(true);
-
-  //   const res = await fetch('/api/subscribe', {
-  //     body: JSON.stringify({
-  //       email: inputEl.current.value
-  //     }),
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     method: 'POST'
-  //   });
-
-  //   setLoading(false);
-  //   try {
-
-  //       const { error } = await res.json();
-    
-  //       console.log("ouch2")
-    
-    
-  //       if (error) {
-  //         toast({
-  //           title: 'An error occurred.',
-  //           description: error,
-  //           status: 'error',
-  //           duration: 3000,
-  //           isClosable: true
-  //         });
-    
-  //         return;
-  //       }
-    
-  //      // trackGoal('JYFUFMSF', 0);
-  //       inputEl.current.value = '';
-  //       toast({
-  //         title: 'Success!',
-  //         description: 'You are now subscribed.',
-  //         status: 'success',
-  //         duration: 3000,
-  //         isClosable: true
-  //       });
-
-  //     } 
-  //     catch (error) {
-          
-  //       toast({
-  //           title: 'An error occurred.',
-  //           description: "error.message.rejectedresponse",
-  //           status: 'error',
-  //           duration: 3000,
-  //           isClosable: true
-  //         });
-
-  //       console.error(error);
-  //     }
-  
-  // };
-  //   setLoading(false);
-  //   const { error } = await res.json();
-
-  //   if (error) {
-  //     toast({
-  //       title: 'An error occurred.',
-  //       description: error,
-  //       status: 'error',
-  //       duration: 3000,
-  //       isClosable: true
-  //     });
-
-  //     return;
-  //   }
-
-  //   trackGoal('JYFUFMSF', 0);
-  //   inputEl.current.value = '';
-  //   toast({
-  //     title: 'Success!',
-  //     description: 'You are now subscribed.',
-  //     status: 'success',
-  //     duration: 3000,
-  //     isClosable: true
-  
 
   return (
     <SubscribeWrapper>
-     <form 
-     name="contact" 
+     <form netlify name="contact"
      method="POST" 
      data-netlify="true" 
      data-netlify-honeypot="bot-field"
      action="https://cwmoon.com"
      >
-    <input type="hidden" name="form-name" value="contact" />
+    <input 
+    type="hidden" 
+    name="form-name" 
+    value="contact" />
 
      <H2>
     Subscribe to the newsletter
