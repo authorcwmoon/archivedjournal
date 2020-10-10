e44975deedc563b439cf7816d4fcc6a19367f85d
Christian Williams <christian@MacBook-Pro.local>
2020-10-10 14:40:54 -0500

    net

--- src/templates/allPosts.js
+++ src/templates/allPosts.js
@@ -51,18 +51,6 @@ const allPosts = ({ pageContext, data }) => {
         prevPage={prevPage}
         nextPage={nextPage}
       />
-
-      <form name="contact" netlify>
-        <p>
-          <label>Name <input type="text" name="name" /></label>
-        </p>
-        <p>
-          <label>Email <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
       <Subscribe />
     </Container>
   )
