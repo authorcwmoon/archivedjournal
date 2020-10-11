0e45249347803be8525807b99e601d8fd53d3de5
Christian Williams <christian@MacBook-Pro.local>
2020-10-11 09:10:11 -0500

    chakra-plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,19 +9,20 @@ module.exports = {
   },
   plugins: [
     {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `pages`,
-    //     path: `${__dirname}/src/pages`,
-    //   },
-    // },
-    // {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
         path: `${__dirname}/src/posts`,
       },
     },
+
+    {
+      resolve: `gatsby-plugin-chakra-ui`,
+
+    },
+
+
+
     {
       resolve: `gatsby-source-filesystem`,
       options: {
